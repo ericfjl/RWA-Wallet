d1fd61b0ad7b3b8d72bacc863e5abaaae5311c78
Web3HackerWorld <web3hackerdao@gmail.com>
2023-07-08 22:12:57 +0800

    ✨ add nft item page

---
+++ src/options/pages/book/[tokenId]/[itemSlug].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <div bg-white>
+    <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+      <LandingItemBreadcrumb py-10 />
+      <LandingItemImgs />
+      <LandingItemText />
+      <LandingFooter />
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: landing
+</route>
